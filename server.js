335dcfc39466bb7ad2adc659b9cfa2c6e61224bd
Kris <kris7b7d@gmail.com>
2018-05-13 12:42:55 +0700

    Heroku support

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
