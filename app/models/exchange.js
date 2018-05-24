c80241e3c0dc85e792c62185546c04200528dfbb
Kris <kris7b7d@gmail.com>
2018-05-24 17:59:50 +0700

    add running date to model

--- app/models/exchange.js
+++ app/models/exchange.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose');
 
 var Exchange = mongoose.model('Exchange', {
+  source: {
+    type: String,
+    trim: true,
+    default: "api" //[api, cli]
+  },
+  age: {
+    type : Date,
+    default: Date.now
+  },
   base: {
     type: String,
     required: true,
     trim: true,
+    uppercase: true,
     minlength: 1
   },
   rates: {
