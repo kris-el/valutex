3322b4e03547975167484a39fb9de00b2ef3ecdc
Kris <kris7b7d@gmail.com>
2018-05-25 02:00:39 +0700

    cli function

---
+++ app/cli.js
@@ -0,0 +1,19 @@
+var config = require('./config');
+const currency = require('./currency');
+var {mongoose} = require('./mongoose');
+var {Exchange} = require('./models/exchange');
+
+currency.getData(process.env.FIXER_KEY).then((data) => {
+  data.source = "cli";
+  var newExchange = new Exchange(data);
+  newExchange.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 4));
+    mongoose.connection.close();
+  }, (e) => {
+    console.log('Error saving the exchange rates!');
+    mongoose.connection.close();
+  });
+}, (errorMessage) => {
+  console.log('Error getting the exchange rates!');
+  mongoose.connection.close();
+});
