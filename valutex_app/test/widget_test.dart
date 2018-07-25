a733587f24c3d3eced55f2d84a6c988f62a8dcb0
Kris <kris7b7d@gmail.com>
2018-07-25 12:18:35 +0700

    Icon added to the project

--- valutex_app/test/widget_test.dart
+++ valutex_app/test/widget_test.dart
@@ -7,7 +7,7 @@
 //import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:valutex_vibe/main.dart';
+import 'package:valutex_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
